f446d4fb7302d62df95bebc04a27a65ce9b97e8c
tirth_bal <tirthprakashbal@gmail.com>
2019-04-27 14:39:40 +0530

    Add personal portfolio

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="app" style="margin-top:0;">
+        <section class="hero is-fullheight" style="margin-top:0; ">
+            <div class="hero-head">
+            <navbar></navbar>
+            <!-- header component  -->
+            </div>
+            <div class="hero-body">
+                <div class="container">
+                    <transition name="moveInUp">
+                    <router-view></router-view>
+                    </transition>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+<style lang="css">
+    @import "../node_modules/bulma/css/bulma.css";
+    .moveInUp-enter-active{
+        animation: fadeIn 2s ease-in;
+    }
+    @keyframes fadeIn{
+        0%{
+            opacity: 0;
+        }
+        50%{
+            opacity: 0.5;
+        }
+        100%{
+            opacity: 1;
+        }
+    }
+    .moveInUp-leave-active{
+        animation: moveInUp .3s ease-in;
+    }
+    @keyframes moveInUp{
+        0%{
+            transform: translateY(0);
+        }
+        100%{
+            transform: translateY(-400px);
+        }
+    }
+</style>
+<script>
+    import Navbar from "./components/Header.vue";
+
+    export default {
+        name: "app",
+        components: {
+            Navbar
+        }
+    };
+</script>
+
